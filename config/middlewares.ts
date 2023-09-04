8e9c685e8a0597444a95197cbd0f85411d4f9104
vi <vitalya.permyakov155@gmail.com>
2023-09-04 14:26:54 +0700

    feat: s3 bucket connect

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,69 @@
-export default [
+// export default [
+//   'strapi::errors',
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           'connect-src': ["'self'", 'https:'],
+//           'img-src': [
+//             "'self'",
+//             'data:',
+//             'blob:',
+//             'market-assets.strapi.io',
+//             'https://s3.appctl.ru',
+//           ],
+//           'media-src': [
+//             "'self'",
+//             'data:',
+//             'blob:',
+//             'market-assets.strapi.io',
+//             'https://s3.appctl.ru',
+//           ],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+module.exports = ({ env }) => ([
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('S3_ENDPOINT'),
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('S3_ENDPOINT'),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +72,4 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]);
--- config/plugins.ts
+++ config/plugins.ts
@@ -6,5 +6,21 @@ module.exports = ({ env }) => ({
   'publisher': {
 		enabled: true
 	},
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('S3_ACCESS_KEY_ID'),
+        secretAccessKey: env('S3_ACCESS_SECRET'),
+        endpoint: env('S3_ENDPOINT'),
+        region: env('S3_REGION'),
+        s3BucketEndpoint: true,
+        s3ForcePathStyle: true,
+        params: {
+          Bucket: env('S3_BUCKET'),
+        },
+      },
+    },
+  },
   // ...
 });
